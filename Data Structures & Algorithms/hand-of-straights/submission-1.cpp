e5d7740a2e5375de39792fe7208867fab23a0fd4
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-04-30 16:08:47 +0200

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize) return false;
+        unordered_map<int, int> count;
+        for (int h : hand) count[h]++;
+        for (int h : hand) {
+            if (count[h]) {
+                int start = h;
+                while (count[--start]) {}
+                for (int i = ++start; i < start + groupSize; ++i) {
+                    if (count[i]-- <= 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
