c4e8a719a32ec9b92b4b3ef20880662db02b6870
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-05-04 23:34:36 +0200

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for (int s : stones) maxHeap.push(s);
+        while (maxHeap.size() > 1){
+            int s1 = maxHeap.top();
+            maxHeap.pop();
+            int s2 = maxHeap.top();
+            maxHeap.pop();
+            if (s1 != s2) maxHeap.push(abs(s1 - s2));
+        }
+        return maxHeap.size() ? maxHeap.top() : 0;
+    }
+};
