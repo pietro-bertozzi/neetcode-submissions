f04258d4e0e89a5bcbea82dd6e195ca9cd1677ab
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-05-05 23:52:38 +0200

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    pair<int, int> partition(vector<int>& nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low;
+        for (int j = low; j < high; ++j) {
+            if (nums[j] > pivot) {
+                swap(nums[j], nums[i++]);
+            }
+        }
+        swap(nums[high], nums[i]);
+        return {i, pivot};
+    }
+
+    int quickSelect(vector<int>& nums, int low, int high, int k) {
+        pair<int, int> index_and_pivot = partition(nums, low, high);
+        int index = index_and_pivot.first;
+        int pivot = index_and_pivot.second;
+        if (index > k) return quickSelect(nums, low, index - 1, k);
+        else if (index < k) return quickSelect(nums, index + 1, high, k);
+        else return pivot;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        return quickSelect(nums, 0, nums.size() - 1, k - 1);
+    }
+};
